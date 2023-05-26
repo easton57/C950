d38e5ba0dea459dd3f2b7a45709dca65948b8aab
Easton Seidel <eseidel1357@gmail.com>
2023-05-25 21:37:48 -0700

    Corrected to use array instead of dictionary

--- truck.py
+++ truck.py
@@ -3,13 +3,12 @@
 class Truck:
     def __init__(self, truck_number):
         self.number = truck_number
-        self.packages = []  # Dictionary for ID: location, max 16
+        self.packages = []
         self.drivers = 2
 
     def add_package(self, new_package, location, special_note=False):
-        if len(self.packages) > 16:
+        if len(self.packages) == 16:
             return False
         else:
-            self.packages[new_package] = location
-            self.special_note[new_package] = special_note
+            self.packages.append(new_package)
             return True
