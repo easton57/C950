f3e25c18e13ebde1944ad654bcf5eb399a94eb2b
Easton Seidel <eseidel1357@gmail.com>
2023-07-17 17:26:24 -0700

    Brought over the good stuff

---
+++ new_main.py
@@ -0,0 +1,65 @@
+# Easton Seidel: 007806406
+import math
+import pandas as pd
+from graphs import Graph
+from truck import Truck
+from hashbrown import HashIt, Packages
+
+
+def main():
+    # Define our trucks
+    total_trucks = 3
+    trucks = []
+
+    for i in range(total_trucks):
+        trucks.append(Truck())
+
+    # Simulation variable, False, follows true time of day, True, 1 minute = 1 second
+    sim_routes = True
+
+    # Define our packages hashmap
+    all_packages = Packages()
+
+    # import our xlsx files
+    distances = pd.read_excel("./project_files/WGUPS Distance Table.xlsx")
+    packages = pd.read_excel("./project_files/WGUPS Package File.xlsx")
+
+    # Get what we care about
+    distances_df = pd.DataFrame(distances).iloc[7:]
+    packages_df = pd.DataFrame(packages).iloc[7:]
+
+    distances_headers = pd.DataFrame(distances).iloc[6]
+    packages_headers = pd.DataFrame(packages).iloc[6]
+
+    distances_df = distances_df.set_axis(distances_headers, axis=1)
+    packages_df = packages_df.set_axis(packages_headers, axis=1)
+
+    # Plug packages into our hashmap
+    for i in range(len(packages_df.index)):
+        row = packages_df.iloc[i]
+
+        all_packages.set_package(row[0], row[1], row[5], row[2], row[4], row[6], "Undelivered", row[7])
+
+    # Make a graph with our locations
+    stop_map = Graph(len(distances_df.index))
+
+    # add the vertices
+    for i in range(len(distances_df.index)):
+        row = distances_df.iloc[i]
+
+        stop_map.add_vertex(row[0].replace('\n', ' '))
+
+    # add the edges
+    for i in range(len(distances_df.index)):
+        row = distances_df.iloc[i]
+
+        for j in range(2, 29):
+            if not math.isnan(row[j]):
+                stop_map.add_edge(row[0].replace('\n', ' '), distances_headers.iloc[j].replace('\n', ' '), row[j])
+
+    # Create package lists based on constraints
+
+
+
+if __name__ == "__main__":
+    main()
