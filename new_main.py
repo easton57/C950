eb07f807e95fce1dc605d8ad01299a78b0d64f11
Easton Seidel <eseidel1357@gmail.com>
2023-07-23 17:50:38 -0700

    New filtering algo based on what's in main.

--- new_main.py
+++ new_main.py
@@ -67,6 +67,66 @@ def main():
             if not math.isnan(row[j]):
                 stop_map.add_edge(row[0], distances_headers.iloc[j].replace('\n', ' '), row[j])
 
+    # filter some stuff
+    size = all_packages.size()
+
+    delayed = []
+    paired = []
+    time_critical = []
+    truck_specific = []
+    wrong_address = []
+
+    for i in range(total_trucks):
+        truck_specific.append([])
+
+    for i in range(1, size + 1):
+        curr_pack = all_packages.get_package(i)
+
+        if type(curr_pack.get("Delivery Deadline")) is not str:  # Delivered by a specific time?
+            time_critical.append(curr_pack)
+
+        try:
+            if "Delayed" in curr_pack.get("Special Note"):  # Delayed oh gosh
+                delayed.append(all_packages.get_package(i).get("Package ID"))
+
+            elif "truck" in curr_pack.get("Special Note"):  # Need a specific truck?
+                # Get the truck number and add to the truck_specific slot
+                num = int(curr_pack.get("Special Note")[-1])
+                truck_specific[num - 1].append(all_packages.get_package(i).get("Package ID"))
+
+            elif "Must be delivered with" in curr_pack.get("Special Note"):  # What packages need to be paired?
+                deliv_list = curr_pack.get("Special Note").replace(",", "").split(" ")[4:]
+                deliv_list = [int(id) for id in deliv_list]
+
+                package_id = all_packages.get_package(i).get("Package ID")
+
+                paired.append([package_id] + deliv_list)
+
+            elif "Wrong address" in curr_pack.get("Special Note"):
+                wrong_address.append(all_packages.get_package(i).get("Package ID"))
+
+        except Exception as e:
+            print(f"Package with id {i} has no notes! {e}")
+            size += 1
+
+    # some var cleanup for my sake
+    del curr_pack, distances_headers, packages_headers, distances, packages, size, row, i, j, num, package_id, deliv_list
+
+    # Merge paired packages list's where things are common
+    for i in paired:
+        for j in i:
+            list_size = len(paired)
+
+            for k in paired:
+                if j in k and i != k:
+                    paired.append(list(set(i).union(set(k))))
+                    paired.remove(i)
+                    paired.remove(k)
+                    break
+
+            if list_size > len(paired):
+                break
+
     # Create the routes
 
     pass
